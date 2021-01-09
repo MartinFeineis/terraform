f8a4a78111a7930b1180f85a6e82b620967bdc2e
Martin Feineis <martifein@gmail.com>
2021-01-09 18:57:54 -0500

    db remote state

--- backend.tf
+++ backend.tf
@@ -1,6 +1,3 @@
-
-
-
 resource "aws_s3_bucket" "remote_state_bucket" {
   bucket = "villapenguinremotestatebucket"
   acl    = "private"
@@ -11,12 +8,29 @@ resource "aws_s3_bucket" "remote_state_bucket" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "villapenguinremotestatebucket"
-    key            = "circleci/remotestate"
-    region         = "us-east-1"
-    dynamodb_table = "villapenguintfrmstate"
-    encrypt        = true
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+ 
+  tags {
+    Name = "TerraformRemoteState"
+    terraformed = true
   }
 }
+
+#terraform {
+#  backend "s3" {
+#    bucket         = "villapenguinremotestatebucket"
+#    key            = "circleci/remotestate"
+#    region         = "us-east-1"
+#    dynamodb_table = "villapenguintfrmstate"
+#    encrypt        = true
+#  }
+#}
