cb124d5961302ac87f6395a5a8a5e76530671442
Martin Feineis <martifein@gmail.com>
2021-01-10 11:44:55 -0500

    iaming

---
+++ wasserraptor/iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_user" "iam_wrp" {
+  name = "wasserraptor"
+
+  tags = {
+    Name        = "IAMWasserRaptor"
+    terraformed = true
+  }
+}
+
+resource "aws_iam_policy" "iam_policy_wrp" {
+  name        = "WasserraptorPolicy"
+  description = "Policy for Wasserraptor account to s3 bucket"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": "s3:ListAllMyBuckets",
+        "Resource": "arn:aws:s3:::*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "s3:ListBucket",
+            "s3:GetBucketLocation"
+        ],
+        "Resource": "arn:aws:s3:::wasserraptorde"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+            "s3:GetObject",
+            "s3:GetObjectAcl",
+            "s3:DeleteObject"
+        ],
+        "Resource": "arn:aws:s3:::wasserraptorde/*"
+    }
+  ]
+}
+EOF
+}
