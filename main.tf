0d1324a75b75160899155f1ce0756e8b9e707527
Martin Feineis <martifein@gmail.com>
2021-01-10 14:55:49 -0500

    outputting in main test

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ output "main_secret" {
   description = "Secret Key for IAM User in main"
   sensitive   = true
 }
+
+output "testouput" {
+  value = "Test String"
+}
+
 #resource "aws_instance" "web" {
 #  ami           = "ami-0be2609ba883822ec"
 #  instance_type = "t3.micro"
