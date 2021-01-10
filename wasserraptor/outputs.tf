892a415846b92066203a50d340267c43ccaf0da8
Martin Feineis <martifein@gmail.com>
2021-01-10 13:51:46 -0500

    formatting

--- wasserraptor/outputs.tf
+++ wasserraptor/outputs.tf
@@ -1,6 +1,6 @@
-output "aws_iam_access_key" {
-  value = aws_iam_access_key.iam_ackey_wrp.name
-}
+#output "aws_iam_access_key" {
+#  value = aws_iam_access_key.iam_ackey_wrp.name
+#}
 
 output "secret" {
   value       = aws_iam_access_key.iam_ackey_wrp.encrypted_secret
