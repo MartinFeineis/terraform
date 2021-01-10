0e3a4709c2fed90508a059371e65fac2ad8e4087
Martin Feineis <martifein@gmail.com>
2021-01-10 12:17:51 -0500

    unmanaginf backend

--- backend.tf
+++ backend.tf
@@ -1,29 +1,29 @@
-resource "aws_s3_bucket" "remote_state_bucket" {
-  bucket = "villapenguinremotestatebucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    terraformed = true
-  }
-}
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "TerraformRemoteState"
-    terraformed = true
-  }
-}
+#resource "aws_s3_bucket" "remote_state_bucket" {
+#  bucket = "villapenguinremotestatebucket"
+#  acl    = "private"
+#
+#  tags = {
+#    Name        = "My bucket"
+#    terraformed = true
+#  }
+#}
+#
+#resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+#  name           = "terraform-state-lock-dynamo"
+#  hash_key       = "LockID"
+#  read_capacity  = 20
+#  write_capacity = 20
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#
+#  tags = {
+#    Name        = "TerraformRemoteState"
+#    terraformed = true
+#  }
+#}
 
 terraform {
   backend "s3" {
