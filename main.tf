758525fff60959142c97e3c4db4ac4ca77a580c0
Martin Feineis <martifein@gmail.com>
2021-01-09 20:02:19 -0500

    done for today

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
   secret_key = var.secretkey
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0be2609ba883822ec"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name        = "HelloWorld"
-    terraformed = true
-  }
-}
+#resource "aws_instance" "web" {
+#  ami           = "ami-0be2609ba883822ec"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name        = "HelloWorld"
+#    terraformed = true
+#  }
+#}
