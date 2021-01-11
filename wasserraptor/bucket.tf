6389fa178d8f61c0e2e0aacccb4869dbbec3b9a0
Martin Feineis <martifein@gmail.com>
2021-01-10 22:14:41 -0500

    json issue

--- wasserraptor/bucket.tf
+++ wasserraptor/bucket.tf
@@ -2,21 +2,21 @@ resource "aws_s3_bucket" "wasserraptor_bucket" {
   bucket = "wasserraptorde"
   acl    = "public-read"
   policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::example.com/*"
-            ]
-        }
-    ]
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+              "s3:GetObject"
+          ],
+          "Resource": [
+              "arn:aws:s3:::wasserraptorde/*"
+          ]
+      }
+  ]
 }
 EOF
   website {
