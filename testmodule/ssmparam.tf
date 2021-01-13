84b393ed27227398e5baa9969e461d9db4db9772
Martin Feineis <martifein@gmail.com>
2021-01-12 23:04:26 -0500

    developing tags 5

--- testmodule/ssmparam.tf
+++ testmodule/ssmparam.tf
@@ -1,15 +1,15 @@
 variable "commontags" {}
 
 resource "aws_ssm_parameter" "dev_param" {
-  name        = "Development Parameter"
+  name        = "Development/Parameter"
   description = "The parameter description"
   value       = "TestThis"
-type = "String"
-  tags = merge ({
+  type        = "String"
+  tags = merge({
     environment = "production"
   }, var.commontags)
 }
 
 output "paramoutput" {
- value = aws_ssm_parameter.dev_param.arn
+  value = aws_ssm_parameter.dev_param.arn
 }
