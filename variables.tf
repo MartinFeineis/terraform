cf1006fb238436992abae58ee52cd2e64e9fa47d
Martin Feineis <martifein@gmail.com>
2021-01-12 22:31:01 -0500

    developing tags 2

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "accesskey" {}
 variable "secretkey" {}
 
-locals {
-    module = basename(abspath(path.module))
-}
+#locals {
+#    module = basename(abspath(path.module))
+#}
 
 
 variable "commontags" {
   description = "Commona Tags applicable to all objects"
   default = { terraformed = true,
-    Name   = local.Name,
-    Module = local.module
+    Name   = "NameVar",
+    Module = 1
   }
 }
