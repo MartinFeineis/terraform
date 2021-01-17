aee25a69c60c13967948c3a665156d06730d0591
Martin Feineis <martifein@gmail.com>
2021-01-16 19:08:37 -0500

    formatting 1

--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@ variable "secretkey" {}
 #    module = basename(abspath(path.module))
 #}
 variable "ecrnames" {
-  type = list(string)
-  default = ["nginx", "flask", "django", "msgapi"] 
+  type    = list(string)
+  default = ["nginx", "flask", "django", "msgapi"]
 }
 variable "roottags" {
   description = "Commona Tags applicable to all objects"
