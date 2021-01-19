001ff6522ace681d5faafd63dd5cf227f6919dfe
Martin Feineis <martifein@gmail.com>
2021-01-18 20:23:24 -0500

    testing

--- eksaws/vpc.tf
+++ eksaws/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "education-vpc"
+  name                 = "eks-aws-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -17,17 +17,22 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  tags = merge(var.roottags, var.moduletags, {
+
+  tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    },
+  }
 
-    { public_subnet_tags = {
-      "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-      "kubernetes.io/role/elb"                      = "1"
-    } },
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
 
-    { private_subnet_tags = {
-      "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-      "kubernetes.io/role/internal-elb"             = "1"
-  } })
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+output "azs" {
+  value = data.aws_availability_zones.available
 }
--- testmodule/ssmparam.tf
+++ testmodule/ssmparam.tf
@@ -2,7 +2,6 @@ locals {
   module = basename(abspath("."))
 }
 
-
 resource "aws_ssm_parameter" "dev_param" {
   name        = "/Development/Parameter"
   description = "The parameter description"
