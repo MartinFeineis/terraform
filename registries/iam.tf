c5a265475fbf93ed3721a943681072e89bb3f741
Martin Feineis <martifein@gmail.com>
2021-01-25 20:31:56 -0500

    iam test

---
+++ registries/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_user" "iam_ecr_pusher" {
+  name = "ecrpusher"
+  
+tags = merge(
+    var.moduletags
+  , var.roottags)
+}
+
+resource "aws_iam_access_key" "iam_ackey_wrp" {
+  user = aws_iam_user.iam_wrp.name
+}
+
+resource "aws_iam_policy" "iam_polics_ecrpusher" {
+  name        = "ECR-Pusher_policy"
+  description = "Policy to push ECR Repositories"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action":  [ "sts:AssumeRoles", "iam:ListRoles"],
+        "Resource": "*"
+    }
+  ]
+}
+EOF
+}
