5510e3379fd6f35e42f7e4a230ac6f786dab48ef
Martin Feineis <martifein@gmail.com>
2021-02-17 20:24:02 -0500

    egress rule

--- truetickets/main.tf
+++ truetickets/main.tf
@@ -86,6 +86,12 @@ resource "aws_security_group" "sshhttphttps" {
    protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
  }
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "truetickets-key" {
