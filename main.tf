73bf6a5aef3527ae0677783c1b769a51387cf207
Martin Feineis <martifein@gmail.com>
2021-04-23 10:21:00 -0400

    commented wasserraptor

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
   secret_key = var.secretkey
 }
 
-module "wasserraptor" {
-  source = "./wasserraptor"
-}
+#module "wasserraptor" {
+#  source = "./wasserraptor"
+#}
 
 module "orgs" {
   source = "./accounts"
