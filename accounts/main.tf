6a573a05c8fc664a6ab0d76fcf01e6993b6bc5de
Martin Feineis <martifein@gmail.com>
2021-04-23 15:52:31 -0400

    add org unit

--- accounts/main.tf
+++ accounts/main.tf
@@ -1,3 +1,12 @@
+resource "aws_organizations_organization" "martinorg" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+  ]
+
+  feature_set = "ALL"
+}
+
 resource "aws_organizations_account" "aolcom" {
   name  = "aol-test-account"
   email = "martifein@aol.com"
