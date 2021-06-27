12f1f57cf32531058fd68c046dcfccbd188ab1fd
Martin Feineis <martinfeineis@Martins-MBP.fios-router.home>
2021-06-27 18:38:01 -0400

    mail bcuekt

--- scps/main.tf
+++ scps/main.tf
@@ -13,6 +13,16 @@
 #CONTENT
 #  tags    = merge(var.roottags, var.moduletags)
 #}
+resource "aws_s3_bucket" "nxtgenbucket" {
+  bucket = "nxtgenenginemails"
+  acl    = "private"
+
+  tags = {
+    Name        = "MailBucket"
+    Environment = "Dev"
+  }
+}
+
 
 resource "aws_route53_record" "nxtgenmxrec" {
   zone_id = "Z2LBEWPVZUG9W2"
@@ -20,4 +30,18 @@ resource "aws_route53_record" "nxtgenmxrec" {
   type    = "MX"
   ttl     = "300"
   records = ["10 inbound-smtp.us-east-1.amazonaws.com"]
+}
+
+resource "aws_ses_receipt_rule" "store" {
+  name          = "store"
+  rule_set_name = "default-rule-set"
+  recipients    = ["*@nxtgenengines.com"]
+  enabled       = true
+  scan_enabled  = true
+
+  s3_action {
+    bucket_name = aws_s3_bucket.nxtgenbucket.name
+    object_key_prefix = "incomingMails"
+    position    = 2
+  }
 }
