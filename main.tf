847a2a55b9722b5918b0b591ec8c194a7d8ac51b
Martin Feineis <martifein@gmail.com>
2022-06-14 23:24:12 -0400

    root-ided

--- main.tf
+++ main.tf
@@ -20,5 +20,5 @@ module "scps" {
 
 resource "aws_organizations_organizational_unit" "dev_org" {
   name      = "development"
-  parent_id = aws_organizations_organization.dev_org.roots[0].id
+  parent_id = "r-7ze0"
 }
