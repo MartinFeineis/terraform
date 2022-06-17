4311c4560224208d357750a290ce3f4c6e08ec47
Martin Feineis <martifein@gmail.com>
2022-06-17 11:11:40 -0400

    only ou for scps

--- scps/region.tf
+++ scps/region.tf
@@ -4,45 +4,8 @@ data "aws_iam_policy_document" "region_restriction" {
   statement {
     sid = "DenyRegionUsage"
 
-    not_actions = [
-      "a4b:*",
-      "acm:*",
-      "aws-marketplace-management:*",
-      "aws-marketplace:*",
-      "aws-portal:*",
-      "budgets:*",
-      "ce:*",
-      "chime:*",
-      "cloudfront:*",
-      "config:*",
-      "cur:*",
-      "directconnect:*",
-      "ec2:DescribeRegions",
-      "ec2:DescribeTransitGateways",
-      "ec2:DescribeVpnGateways",
-      "fms:*",
-      "globalaccelerator:*",
-      "health:*",
-      "iam:*",
-      "importexport:*",
-      "kms:*",
-      "mobileanalytics:*",
-      "networkmanager:*",
-      "organizations:*",
-      "pricing:*",
-      "route53:*",
-      "route53domains:*",
-      "s3:GetAccountPublic*",
-      "s3:ListAllMyBuckets",
-      "s3:PutAccountPublic*",
-      "shield:*",
-      "sts:*",
-      "support:*",
-      "trustedadvisor:*",
-      "waf-regional:*",
-      "waf:*",
-      "wafv2:*",
-      "wellarchitected:*"
+    actions = [
+      "*",
     ]
 
     resources = [
@@ -52,7 +15,7 @@ data "aws_iam_policy_document" "region_restriction" {
     effect = "Deny"
 
     condition {
-      test     = "StringNotEqualsIgnoreCase"
+      test     = "StringEqualsIgnoreCase"
       variable = "aws:RequestedRegion"
       values = var.region_lockdown
     }
